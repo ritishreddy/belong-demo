748700c82903c50d339df6a256c1148358753223
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-10 11:39:12 +1100

    Update jumphost.tf

--- jumphost.tf
+++ jumphost.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2jumphost" {
   instance_type = "t2.micro"
-  ami = "ami-03d8059563982d7b0" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
+  ami = "ami-0310483fb2b488153" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
   subnet_id = aws_subnet.nat_gateway.id
   security_groups = [aws_security_group.securitygroup.id]
   key_name = aws_key_pair.ssh.key_name
