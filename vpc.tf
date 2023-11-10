8cec28725ca2a43d396fd62b45ecabd7bd1b5cc0
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-10 11:14:16 +1100

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    "Name" = "Demo"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "instance" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  cidr_block = "10.0.1.0/24"
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    "Name" = "DemoSubnetInstance"
+  }
+}
