39807020a43d47483cf7a02299e236f9643b9096
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-11 15:31:55 +1100

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -4,6 +4,6 @@ sudo yum install wget unzip httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime
-sudo mkdir /var/www/html
-sudo yum install -y aws-cli
-aws s3 cp s3://belong-coding-challenge/belong-test.html /var/www/html/
+wget https://belong-coding-challenge.s3-ap-southeast-2.amazonaws.com/belong-test.html
+sudo mv belong-test.html /var/www/html/
+sudo systemctl restart httpd
