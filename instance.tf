4f5a5075895def072497c02dc29011a1be4979c3
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-11 17:41:13 +1100

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,13 +4,13 @@ resource "tls_private_key" "ssh" {
 }
 
 resource "aws_key_pair" "ssh" {
-  key_name = "DemoMachine"
+  key_name   = "DemoMachine"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
 output "ssh_private_key_pem" {
   sensitive = true
-  value = tls_private_key.ssh.private_key_pem
+  value     = tls_private_key.ssh.private_key_pem
 }
 
 output "ssh_public_key_pem" {
@@ -18,14 +18,14 @@ output "ssh_public_key_pem" {
 }
 
 resource "aws_security_group" "securitygroup" {
-  name = "DemoSecurityGroup"
+  name        = "DemoSecurityGroup"
   description = "DemoSecurityGroup"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.vpc.id
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
   }
   ingress {
     from_port   = 80
@@ -35,9 +35,9 @@ resource "aws_security_group" "securitygroup" {
   }
   egress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
   }
   tags = {
     "Name" = "DemoSecurityGroup"
@@ -45,14 +45,14 @@ resource "aws_security_group" "securitygroup" {
 }
 
 resource "aws_instance" "ec2instance" {
-  instance_type = "t2.micro"
-  ami = "ami-07b5c2e394fccab6e" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
-  subnet_id = aws_subnet.private_instance.id
-  security_groups = [aws_security_group.securitygroup.id]
-  key_name = aws_key_pair.ssh.key_name
+  instance_type           = "t2.micro"
+  ami                     = "ami-07b5c2e394fccab6e" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
+  subnet_id               = aws_subnet.instance.id
+  security_groups         = [aws_security_group.securitygroup.id]
+  key_name                = aws_key_pair.ssh.key_name
   disable_api_termination = false
-  ebs_optimized = false
-  user_data     = file("init_script.sh")
+  ebs_optimized           = false
+  user_data               = file("init_script.sh")
   root_block_device {
     volume_size = "10"
   }
@@ -65,22 +65,22 @@ output "instance_private_ip" {
   value = aws_instance.ec2instance.private_ip
 }
 resource "aws_instance" "ec2instance1" {
-  instance_type = "t2.micro"
-  ami = "ami-07b5c2e394fccab6e" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
-  subnet_id = aws_subnet.private_instance.id
-  security_groups = [aws_security_group.securitygroup.id]
-  key_name = aws_key_pair.ssh.key_name
+  instance_type           = "t2.micro"
+  ami                     = "ami-07b5c2e394fccab6e" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
+  subnet_id               = aws_subnet.instance.id
+  security_groups         = [aws_security_group.securitygroup.id]
+  key_name                = aws_key_pair.ssh.key_name
   disable_api_termination = false
-  ebs_optimized = false
-  user_data     = file("init_script.sh")
+  ebs_optimized           = false
+  user_data               = file("init_script.sh")
   root_block_device {
-    volume_size = "10"
+    volume_size           = "10"
   }
   tags = {
-    "Name" = "BelongMachinePrivate1"
+    "Name"                = "BelongMachinePrivate1"
   }
 }
 
 output "instance_private_ip1" {
-  value = aws_instance.ec2instance.private_ip
+  value                   = aws_instance.ec2instance.private_ip
 }
