dc75d2e4642dcf3e0e8060e00339b0cb529d3a8f
ritishreddy <37667025+ritishreddy@users.noreply.github.com>
2023-11-11 11:03:20 +1100

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -27,6 +27,12 @@ resource "aws_security_group" "securitygroup" {
     to_port = 22
     protocol = "tcp"
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     cidr_blocks = ["0.0.0.0/0"]
     from_port = 0
@@ -40,7 +46,7 @@ resource "aws_security_group" "securitygroup" {
 
 resource "aws_instance" "ec2instance" {
   instance_type = "t2.micro"
-  ami = "ami-0310483fb2b488153" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
+  ami = "ami-062680d0a2ee357d0" # https://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu)
   subnet_id = aws_subnet.instance.id
   security_groups = [aws_security_group.securitygroup.id]
   key_name = aws_key_pair.ssh.key_name
